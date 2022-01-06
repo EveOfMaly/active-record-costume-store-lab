d75c15eb371e84c65e826f779268a031de8ef254
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-01-06 11:56:18 -0500

    issue with db

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,3 +1,22 @@
 # Create your costume_stores migration here
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+
+
+class CostumeStores < ActiveRecord::Migration[4.2]
+
+    def change
+        create_table :costume_stores do |t|
+            t.string :name 
+            t.string :location 
+            t.integer :costumer_inventory
+            t.integer :num_of_employees
+            t.booleon :still_in_business
+            t.datetime :start_time
+            t.datetime :closing_time
+
+            t.timestamps 
+        end
+    end
+
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,21 @@
 # Create your haunted_houses migration here
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+
+class HauntedHouses < ActiveRecord::Migration[4.2] #inherits from ActiveRecord::Base
+    def change
+        create_table :haunted_houses do |t|
+            t.string :name 
+            t.string :location 
+            t.string :description
+            t.string :theme
+            t.integer :price
+            t.booleon :family_friendly
+            t.datetime :opening_date
+            t.datetime :closing_date
+
+            t.timestamps 
+        end
+    end
+
+end 
